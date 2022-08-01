afebec76ecd1b2241003d18c094f83993886d8e9
Jehad Almaliki <yourgitemail@example.com>
2022-08-01 22:09:15 +0300

    creat store

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
+import store from './store'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+.use(store)
+.mount('#app')
---
+++ vue/src/store/index.js
@@ -0,0 +1,11 @@
+
+import {createStrore} from "vuex";
+const store=createStrore(options, {
+  state:{},
+  getters:{},
+  actions:{},
+  mutations:{},
+  modules:{}
+})
+
+export default store;
