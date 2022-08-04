2f71503ee425814c4f224719f67bcfad9fc08847
Jehad Almaliki <yourgitemail@example.com>
2022-08-05 00:04:15 +0300

    export router

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter,createWebHistory } from "vue-router";
+
+const routes=[];
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+export default router;
