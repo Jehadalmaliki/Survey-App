054fa5f2e173fc63b260d863ef4152822c3eab79
Jehad Almaliki <yourgitemail@example.com>
2022-08-05 22:31:21 +0300

    import pages

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,4 +1,9 @@
 import { createRouter,createWebHistory } from "vue-router";
+import Dashboard from '../views/Dashboard.vue';
+import Login from '../views/Login.vue';
+import Register from '../views/Register.vue';
+
+
 
 const routes=[
   {
