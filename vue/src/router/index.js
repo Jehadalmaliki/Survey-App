66e4e0d0a8575584c3cf7953c4ab291e2b535aa4
Jehad Almaliki <yourgitemail@example.com>
2022-08-05 22:11:59 +0300

    configer routers

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,8 +1,25 @@
 import { createRouter,createWebHistory } from "vue-router";
 
-const routes=[];
+const routes=[
+  {
+    path:'/',
+    name:'Dashboard',
+    component:Dashboard
+  },
+  {
+    path:'/login',
+    name:'Login',
+    component:Login
+  },
+  {
+    path:'/register',
+    name:'Register',
+    component:Register
+  },
+];
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
 export default router;
